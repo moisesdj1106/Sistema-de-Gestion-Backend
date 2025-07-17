084d9ac57bcf32241c44b62bfb8c04a2a4590c9e
moisesdj1106 <moises.becerra2004@gmail.com>
2025-07-16 21:35:52 -0400

    ruta de contraseña

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1001,7 +1001,7 @@ export const solicitarRecuperacion = async (req, res) => {
             [token, expiracion, email]
         );
 
-        const enlace = `https://sistema-de-gestion-backend.onrender.com/restablecer/${token}`;
+        const enlace = `https://sistema-de-gestion-desastres.netlify.app/restablecer/${token}`;
 
         await transporter.sendMail({
             from: '"Soporte" <moises.becerra07@gmail.com>',
