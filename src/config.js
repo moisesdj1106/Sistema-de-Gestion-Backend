d9cbe96817ec731e7848d91c8c156a76127b0515
moisesdj1104 <moises.becerra07@gmail.com>
2026-02-03 01:54:38 -0400

    pdf

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ dotenv.config();
 export const DB_DATABASE = 'SISGESDESN';
 export const DB_HOST = 'localhost';
 export const DB_PASSWORD = 'moises';
-export const DB_PORT = 5432;
+export const DB_PORT = 54432;
 export const DB_USER = 'postgres';
 export const PORT = 4000;*/
 
